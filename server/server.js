bcb0d4eea4d2be72e54f5903524061e7a3116ab5
ML6503 <ml6503@gmail.com>
2023-01-05 20:52:44 +0200

    init: start websockets chat app + add server and client

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import { WebSocketServer } from "ws";
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const ws = new WebSocketServer({port: PORT});
+
+ws.on('connection', client => {
+  client.on('message', (message, isBinary) => {
+    [...ws.clients].filter(c => c !== client).forEach(c => c.send(isBinary ? message.toString() : message));
+  });
+});
